bc26002af310842268dfdda1543a89e199e8231b
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-12-26 17:16:31 -0600

    feat: add templates dir for project
    add html template for first app
    
    add object context to render key in template

--- first_app/views.py
+++ first_app/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-  return HttpResponse("Hello world")
+  my_dict = {
+    'insert_me': "Hello, I am from views.py in first app"
+  }
+  return render(request, 'first_app/index.html', context=my_dict)
