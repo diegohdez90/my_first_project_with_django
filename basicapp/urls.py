a4f28ab94b52bb04581fc22fc060bb603837b7bc
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-12-27 17:49:35 -0600

    refactor: update name parameter value

--- basicapp/urls.py
+++ basicapp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from basicapp import views
 
 urlpatterns = [
-  url(r'^$', views.index, name='index'),
+  url(r'^$', views.index, name='index_basic_app'),
   url('form/', views.form_name_view),
 ]
