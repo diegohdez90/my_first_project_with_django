3647030ecdee187ba4b3ee368936e5e5bcb0a796
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-12-27 14:00:58 -0600

    feat: add form validation to match email

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -1,6 +1,27 @@
 from django import forms
+from django.core import validators
+
 
 class Form(forms.Form):
   name = forms.CharField()
   email = forms.CharField()
+  verify_email = forms.EmailField(label='Enter yout email again!')
   text = forms.CharField(widget=forms.Textarea)
+  botcatcher = forms.CharField(required=False,
+  widget=forms.HiddenInput,
+  validators=[validators.MaxLengthValidator(0)])
+
+  #old botcached 
+  #def clean_botcatcher(self):
+  #  botcatcher = self.cleaned_data['botcatcher']
+  #  if len(botcatcher) > 0:
+  #    raise forms.ValidationError("Gotcha bot")
+  #  return botcatcher
+
+  def clean(self):
+    all_clean_data = super().clean()
+    email = all_clean_data['email']
+    vmail = all_clean_data['verify_email']
+
+    if email != vmail:
+      raise forms.ValidationError('Make sure email match')
